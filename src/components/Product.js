4c7081ffcf7b3fee5529c010f0b29fce48418b49
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-02-21 23:41:12 +0700

    fix practice 01

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import {
+  Figure,
+  Col,
+  Row
+} from 'react-bootstrap'
 
 export default class Product extends Component {
+
+  handleUpdate = () => this.props.handleUpdate(this.props._id)
   render() {
+    const { _id, _title, _votes, _description, _url, _submitterAvatarUrl, _productImageUrl } = this.props
     return (
-      <div>
-        from produk
-      </div>
+      <Col md={3}>
+        <Figure>
+          <Figure.Image
+            width="100%"
+            height={180}
+            alt={_title}
+            src={_productImageUrl}
+          />
+          <Figure.Caption>
+            <p>{_description}</p>
+            <p onClick={this.handleUpdate}>Vote {_votes}</p>
+          </Figure.Caption>
+        </Figure>
+      </Col>
     )
   }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react'
+import {
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap'
 import Product from './Product'
-
 import productList from '../Seed.js'
 
 class ProductList extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ products: productList })
+  }
+
+  handleUpdate = (id) => {
+    const addVote = this.state.products.map((product) => {
+      if (product.id === id) {
+        // return Object.assign({}, product, {
+        //    votes: product.votes + 1
+        // });
+        return { ...product, votes: product.votes + 1 }
+      } else {
+        return product;
+      }
+    });
+
+    this.setState({ products: addVote })
+
+  }
+
   render() {
-    console.log(productList)
+
+    console.log(this.state.products)
+
+    const products = this.state.products.sort((a, b) => (
+      a.votes - b.votes
+    ))
+
+    const productComponent = products.map((item, idx) => (
+      <Product
+        key={idx}
+        _id={item.id}
+        _title={item.title}
+        _votes={item.votes}
+        _description={item.description}
+        _url={item.url}
+        _submitterAvatarUrl={item.submitterAvatarUrl}
+        _productImageUrl={item.productImageUrl}
+        handleUpdate={this.handleUpdate}
+      />
+    ));
+
     return (
-      <div>
-        <Product />
-      </div>
+      <Container>
+        <Row className='p-3'>
+          {productComponent}
+        </Row>
+      </Container>
     )
   }
 }
