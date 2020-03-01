a74343a3e65fd405b59f2489240ed86a5c692309
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-03-01 13:22:39 +0700

    update

--- src/components/timer/TimersDashboard.js
+++ src/components/timer/TimersDashboard.js
@@ -11,31 +11,33 @@ import {
 export default class TimersDashboard extends Component {
 
   state = {
-    timers:[
-      { id: uuid(), 
-        title:'Learn React',
+    timers: [
+      {
+        id: uuid(),
+        title: 'Learn React',
         project: 'Web Domination',
         elapsed: '8986300',
         runningSince: null,
         editFormOpen: false
       },
-      { id: uuid(), 
-        title: 'Learn extreme ironing', 
-        project: 'Web Domination', 
-        elapsed:'3890985', 
-        runningSince:null, 
-        editFormOpen:true 
+      {
+        id: uuid(),
+        title: 'Learn extreme ironing',
+        project: 'Web Domination',
+        elapsed: '3890985',
+        runningSince: null,
+        editFormOpen: true
       }
     ]
   }
-  
+
   handleCreateForm = (value) => {
     this.createTimer(value)
   }
 
   createTimer = (value) => {
     const newValue = value
-    this.setState({timers:this.state.timers.concat(newValue)})
+    this.setState({ timers: this.state.timers.concat(newValue) })
   }
 
   handleEditFormSubmit = (data) => {
@@ -45,14 +47,14 @@ export default class TimersDashboard extends Component {
   updateTimer = (data) => {
     this.setState({
       timers: this.state.timers.map((newData) => {
-        if(newData.id === data.id){
-          // return Object.assign({}, newData, {
-          //   title: newData.title,
-          //   project: newData.project,
-          // });
+        if (newData.id === data.id) {
+          return Object.assign({}, newData, {
+            title: newData.title,
+            project: newData.project,
+          });
 
-          return {...newData, title:newData.title, project:newData.project}
-        }else{
+          // return { ...newData, title: newData.title, project: newData.project }
+        } else {
           return newData
         }
       })
@@ -68,50 +70,52 @@ export default class TimersDashboard extends Component {
   }
 
   onStartClick = (id) => this.handlerStart(id)
-  
-  
+
+
   handlerStart = (id) => {
     const now = Date.now()
     console.log(now)
     this.setState({
       timers: this.state.timers.map(item => {
-        if(item.id === id){
-          return {...item, runningSince:now}
-        }else{
+        if (item.id === id) {
+          return { ...item, runningSince: now }
+        } else {
           return item
         }
       })
     })
   }
-  
+
   onStopClick = (id) => this.handlerStop(id)
 
   handlerStop = (id) => {
     const now = Date.now()
     this.setState({
-      timers: this.state.timers.map(item => {
-        if(item.id !== id){
-          const lastElapsed = now - item.runningSince
-          return {...item, elapsed: item.elapsed + lastElapsed, runningSince:null}
-        }else{
-          return item
+      timers: this.state.timers.map((timer) => {
+        if (timer.id === id) {
+          const lastElapsed = now - timer.runningSince;
+          return Object.assign({}, timer, {
+            elapsed: timer.elapsed + lastElapsed,
+            runningSince: null,
+          });
+        } else {
+          return timer;
         }
-      })
-    })
-
+      }),
+    });
 
   }
 
   render() {
-    
+
     return (
       <ContainerParent>
         <Col xs={12}>
           <h3 className='text-center'><b>Timer app</b></h3>
         </Col>
         <Col xs={12}>
-          <EditableTimerList 
-            timers={this.state.timers} 
+          <EditableTimerList
+            timers={this.state.timers}
             onFormSubmit={this.handleEditFormSubmit}
             deleteList={(id) => this.deleteList(id)}
             onStartClick={(id) => this.onStartClick(id)}
@@ -119,7 +123,7 @@ export default class TimersDashboard extends Component {
           />
         </Col>
         <Col xs={12}>
-          <ToggleableTimerFormat 
+          <ToggleableTimerFormat
             handleCreateForm={(value) => this.handleCreateForm(value)}
           />
         </Col>
